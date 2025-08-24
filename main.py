746de4ebf907dd01cb70e602c29d0f116ca93f68
FerrumVega <ferrumthevega@gmail.com>
2025-08-24 13:08:29 +0300

    if __name__ == "__main__"

--- main.py
+++ main.py
@@ -121,5 +121,6 @@ class MainWindow(QtWidgets.QMainWindow):
         sys.exit(app.exec())
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
